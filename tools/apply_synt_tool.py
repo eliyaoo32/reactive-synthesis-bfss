5ddc5b9c794e225e9ab123659f53a6786bda72d8
Eliyahu basa <eliyaoo32@gmail.com>
2022-12-15 18:14:28 +0200

    Start dev tool

---
+++ tools/apply_synt_tool.py
@@ -0,0 +1,86 @@
+import os
+from subprocess import PIPE, Popen
+from multiprocessing import Pool
+from random import shuffle
+
+from lookup_dependencies import create_folder, get_all_benchmarks, get_benchmark_output_path
+
+
+def process_benchmark(benchmark, timeout, output_dir, synt_tool, algorithm):
+    benchmark_name = benchmark['benchmark_name']
+    input_vars = benchmark['input_vars']
+    output_vars = benchmark['output_vars']
+    ltl_formula = benchmark['ltl_formula']
+
+    print("Processing {}...".format(benchmark_name))
+
+    if synt_tool == 'ltlsynt':
+        cli_cmd = 'timeout --signal=HUP {timeout} {synt_tool} --formula="{formula}" --ins="{inputs}" --outs="{outputs}" --algo={algo}'.format(
+            timeout=timeout, synt_tool=synt_tool, formula=ltl_formula, inputs=input_vars, outputs=output_vars, algo=algorithm)
+    else:
+        raise Exception("Unknown tool {}".format(synt_tool))
+
+    with Popen(cli_cmd, stdout=PIPE, stderr=PIPE, shell=True, preexec_fn=os.setsid) as process:
+        result = process.communicate()[0].decode("utf-8")
+
+    print("Done Processing {}!".format(benchmark_name))
+    with open(get_benchmark_output_path(benchmark_name, output_dir), "w+") as outfile:
+        outfile.write(result)
+
+
+def main():
+    from argparse import ArgumentParser
+    parser = ArgumentParser(add_help=True)
+    parser.add_argument(
+        '--name', help="Filter by benchmarks name", type=str, default='')
+    parser.add_argument(
+        '--benchs_list', help="A path to csv file which store benchmark instances", type=str, required=True)
+    parser.add_argument(
+        '--output_dir', help="Path to the directory which the output files are stored into", type=str, required=True)
+    parser.add_argument(
+        '--timeout', help="Timeout of each benchmark", type=str, default='40m')
+    parser.add_argument('--all', help="Apply altough output file already exists ",
+                        default=False, action='store_true')
+    parser.add_argument(
+        '--workers', help="Number of workers", type=int, default=16)
+    parser.add_argument('--tool', help="Algorithm to use",
+                        type=str, choices=['ltlsynt'], required=True)
+    parser.add_argument('--algorithm', help="Algorithm to use, passed directly to the tool",
+                        type=str, required=False)
+    args = parser.parse_args()
+
+    workers = args.workers
+    benchmark_name_filter = args.name
+    ignore_existing_output = not args.all
+    benchmarks_path = args.benchs_list
+    benchmarks_timeout = args.timeout
+    output_dir = args.output_dir
+    algorithm = args.algorithm
+    synt_tool = args.tool
+
+    """
+    Search for benchmarks by configuration
+    """
+    create_folder(output_dir)
+    benchmarks = get_all_benchmarks(
+        output_dir=output_dir,
+        ignore_if_output_exists=ignore_existing_output,
+        benchmark_name_filter=benchmark_name_filter,
+        benchmarks_path=benchmarks_path
+    )
+    print("Found {} benchmarks.".format(len(benchmarks)))
+    shuffle(benchmarks)
+
+    """
+    Apply the algorithm
+    """
+    process_benchmark_args = [
+        (benchmark, benchmarks_timeout, output_dir, synt_tool, algorithm)
+        for benchmark in benchmarks
+    ]
+    with Pool(workers) as pool:
+        pool.starmap(process_benchmark, process_benchmark_args)
+
+
+if __name__ == "__main__":
+    main()
