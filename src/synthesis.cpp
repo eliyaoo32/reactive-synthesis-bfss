30580c85d62a1788d6f0052eb4b0bb62d69d750f
Eliyahu basa <eliyaoo32@gmail.com>
2022-12-19 18:32:44 +0200

    Synthesis a game from NBA

--- src/synthesis.cpp
+++ src/synthesis.cpp
@@ -25,6 +25,9 @@ twa_graph_ptr ntgba2dpa(const twa_graph_ptr& aut, bool force_sbacc);
 void synthesis_buchi(const twa_graph_ptr& buchi, const bdd& outputs, bool force_sbacc);
 
 int main(int argc, const char* argv[]) {
+    /**
+     * Processing synthesis problem
+     */
     string synt_formula, input_str, output_str;
     bool is_verbose;
     Algorithm selected_algorithm;  // TODO: remove the selected algorithm
@@ -35,8 +38,8 @@ int main(int argc, const char* argv[]) {
         return EXIT_FAILURE;
     }
     SyntInstance synt_instance(input_str, output_str);
+    vector<string> output_vars(synt_instance.get_output_vars());
 
-    // Building Buchi Automaton
     spot::parsed_formula pf = spot::parse_infix_psl(synt_formula);
     if (pf.format_errors(std::cerr)) {
         throw std::runtime_error("Error parsing formula_str: " + synt_formula);
@@ -45,16 +48,77 @@ int main(int argc, const char* argv[]) {
         throw std::runtime_error("Formula is not built yet");
     }
 
-    // option_map& extra_options = gi.opt;
-    // extra_options.operator[]("simul", 0);
-    // extra_options.set_str("tls-impl", 1);
-    // extra_options.set_str("wdba-minimize", 2);
-    // const bdd_dict_ptr& dict = gi.dict;
+    /**
+     * Creates a game
+     */
+    synthesis_info gi;
+    gi.s = synthesis_info::algo::SPLIT_DET;
 
-    // spot::translator trans(extra_options);
+    option_map& extra_options = gi.opt;
+    const bdd_dict_ptr& dict = gi.dict;
+
+    extra_options.set_if_unset("simul", 0);
+    extra_options.set_if_unset("tls-impl", 1);
+    extra_options.set_if_unset("wdba-minimize", 2);
+
+    translator trans(dict, &extra_options);
+    auto aut = trans.run(pf.f);
+    auto tobdd = [&aut](const std::string& ap_name) {
+        return bdd_ithvar(aut->register_ap(ap_name));
+    };
+    auto is_out = [&output_vars](const std::string& ao) -> bool {
+        return std::find(output_vars.begin(), output_vars.end(), ao) != output_vars.end();
+    };
+    bdd outs = bddtrue;
+    for (auto&& aap : aut->ap()) {
+        if (is_out(aap.ap_name())) {
+            outs &= tobdd(aap.ap_name());
+        }
+    }
+
+    // TODO: understand this split_2step logic
+    auto splitted = split_2step(aut, outs, true);
+
+    // TODO: understand what's the function ntgba2dpa and how it works internally
+    auto dpa = ntgba2dpa(splitted, gi.force_sbacc);
+
+    // Transform an automaton into a parity game by propagating players.
+    alternate_players(dpa);
+
+    // Merge states knows about players
+    dpa->merge_states();
+    set_synthesis_outputs(dpa, outs);
+
+    auto& arena = dpa;
+
+    /**
+     * A Game solver, takes a game and creates a strategy
+     */
+    bool is_solved = spot::solve_game(arena, gi);
+    if (!is_solved) {
+        cout << "UNREALIZABLE" << endl;
+        return EXIT_FAILURE;
+    }
+
+    spot::mealy_like ml;
+    ml.success = spot::mealy_like::realizability_code::REALIZABLE_REGULAR;
+    ml.mealy_like = spot::solved_game_to_mealy(arena, gi);
+    // TODO: check what is the should_split
+    bool should_split = false;
+    simplify_mealy_here(ml.mealy_like, 5, should_split);
+
+    spot::print_hoa(cout, ml.mealy_like);
+}
+
+/*
     option_map opts;
-    trans.set_type(spot::postprocessor::Buchi);
-    trans.set_pref(spot::postprocessor::SBAcc);
+    opts.set_if_unset("simul", 0);
+    opts.set_if_unset("tls-impl", 1);
+    opts.set_if_unset("wdba-minimize", 2);
+
+    translator trans(&opts);
+    // trans.set_type(spot::postprocessor::Buchi);
+    // trans.set_pref(spot::postprocessor::SBAcc);
     auto aut = trans.run(pf.f);
 
     // Synthesis the buchi automaton
@@ -65,7 +129,7 @@ int main(int argc, const char* argv[]) {
     }
 
     synthesis_buchi(aut, bdd_outputs, force_sbacc);
-}
+*/
 
 void synthesis_buchi(const twa_graph_ptr& buchi, const bdd& outputs, bool force_sbacc) {
     // TODO: check when we can remove the Buchi automaton of specification
@@ -88,7 +152,8 @@ void synthesis_buchi(const twa_graph_ptr& buchi, const bdd& outputs, bool force_
         return;
     }
 
-    auto mealy = solved_game_to_separated_mealy(dpa);
+    auto mealy = solved_game_to_mealy(dpa);
+    simplify_mealy_here(mealy, 5, true);
     print_hoa(cout, mealy);
 }
 
@@ -107,3 +172,11 @@ twa_graph_ptr ntgba2dpa(const twa_graph_ptr& aut, bool force_sbacc) {
     assert(is_deterministic(dpa));
     return dpa;
 }
+
+/****
+ *
+ * LTLSynt usage examples:
+ * ltl2dpa16 example:
+ * ltlsynt --formula='((Fa & Fb & GFp0) | (FG!p0 & (G!a | G!b))) & G((p0 & !p1) | (!p0
+ * &p1))' --outs='p0,p1' --algo=sd --ins='a,b,c' --verbose
+ */
