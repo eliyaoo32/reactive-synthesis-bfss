2992abc614d9dc8c1863a2f3d2f57933fffdf56d
Eliyahu basa <eliyaoo32@gmail.com>
2022-12-30 15:54:17 +0200

    Extracted synthesis tool to sub tasks

--- src/synthesis.cpp
+++ src/synthesis.cpp
@@ -3,35 +3,22 @@ This file takes a Buchi automaton and synthesis it
 */
 
 #include <iostream>
-#include <spot/misc/optionmap.hh>
-#include <spot/twaalgos/degen.hh>
-#include <spot/twaalgos/determinize.hh>
-#include <spot/twaalgos/game.hh>
-#include <spot/twaalgos/hoa.hh>
-#include <spot/twaalgos/isdet.hh>
 #include <spot/twaalgos/mealy_machine.hh>
-#include <spot/twaalgos/sbacc.hh>
-#include <spot/twaalgos/sccfilter.hh>
-#include <spot/twaalgos/synthesis.hh>
-#include <spot/twaalgos/translate.hh>
 #include <vector>
 
 #include "automaton_algorithm.h"
 #include "synt_instance.h"
+#include "synthesis_utils.h"
 #include "utils.h"
 
 using namespace std;
 using namespace spot;
 
-twa_graph_ptr ntgba2dpa(const twa_graph_ptr& aut, bool force_sbacc);
-
 int main(int argc, const char* argv[]) {
     /**
-     * =================== Step 1: Processing synthesis problem
+     * Process the command line arguments
      */
     CLIOptions options;
-    int synthesis_minimize_lvl = 2;  // I.e, simplication level
-
     int parsed_cli_status = parse_cli(argc, argv, options);
     if (!parsed_cli_status) {
         return EXIT_FAILURE;
@@ -44,154 +31,26 @@ int main(int argc, const char* argv[]) {
 
     SyntInstance synt_instance(options.inputs, options.outputs, options.formula);
     AutomatonSyntMeasure synt_measures(synt_instance);
-    vector<string> output_vars(synt_instance.get_output_vars());
 
-    /**
-     * =================== Step 2: Creates NBA of the specification
-     *                             (And prune redundent)
-     */
     synthesis_info gi;
     gi.s = synthesis_info::algo::SPLIT_DET;
-    gi.minimize_lvl = synthesis_minimize_lvl;
+    gi.minimize_lvl = 2;  // I.e, simplication level
 
-    option_map& extra_options = gi.opt;
-    const bdd_dict_ptr& dict = gi.dict;
-
-    extra_options.set_if_unset("simul", 0);
-    extra_options.set_if_unset("tls-impl", 1);
-    extra_options.set_if_unset("wdba-minimize", synthesis_minimize_lvl);
-
-    synt_measures.start_automaton_construct();
-    translator trans(dict, &extra_options);
-    trans.set_type(spot::postprocessor::Buchi);
-    trans.set_pref(spot::postprocessor::SBAcc);
-
-    auto automaton = trans.run(construct_formula(synt_instance));
-    synt_measures.end_automaton_construct(automaton);
-
-    if (options.skip_dependencies) {
-        verbose << "=> Skipping Automaton Pruning" << endl;
-    } else {
-        verbose << "=> Pruning Automaton before searching dependecies:" << endl;
-
-        synt_measures.start_prune_automaton();
-        automaton = spot::scc_filter_states(automaton);  // Prune automaton
-        synt_measures.end_prune_automaton(automaton);
-    }
-
-    auto tobdd = [&automaton](const std::string& ap_name) {
-        return bdd_ithvar(automaton->register_ap(ap_name));
-    };
-
-    if (options.skip_dependencies) {
-        verbose << "=> Skip finding dependent variables..." << endl;
-    } else {
-        /**
-         * =================== Step 3: Find Dependent Variables
-         */
-        verbose << "=> Finding Dependent Variables" << endl;
-
-        vector<string> dependent_variables, independent_variables;
-        AutomatonAlgorithm automaton_dependencies(synt_instance, synt_measures, automaton,
-                                                  false);
-        automaton_dependencies.find_dependencies(dependent_variables,
-                                                 independent_variables);
-        verbose << "=> Found " << dependent_variables.size() << " dependent variables"
-                << endl;
-
-        // =================== Step 4: Remove dependent variables from the NBA
-        verbose << "=> Remove Dependent Variables" << endl;
-        synt_measures.start_remove_dependent_ap();
-        bdd dependents = bddtrue;
-        for (string& dep_var : dependent_variables) {
-            dependents &= tobdd(dep_var);
-        }
-        // Apply exists operator on all edges
-        for (int i = 0; i < automaton->num_states(); i++) {
-            for (auto& edge : automaton->out(i)) {
-                edge.cond = bdd_exist(edge.cond, dependents);
-            }
-        }
-        // Unregister dependent variables
-        for (string& dep_var : dependent_variables) {
-            int ap = automaton->register_ap(dep_var);
-            automaton->unregister_ap(ap);
-        }
-        synt_measures.end_remove_dependent_ap();
-    }
-
-    /**
-     * =================== Step 5: Build a game from the NBA
-     */
-    synt_measures.start_split_2step();
-    auto is_out = [&output_vars](const std::string& ao) -> bool {
-        return std::find(output_vars.begin(), output_vars.end(), ao) != output_vars.end();
-    };
-    bdd outs = bddtrue;
-    for (auto&& aap : automaton->ap()) {
-        if (is_out(aap.ap_name())) {
-            outs &= tobdd(aap.ap_name());
-        }
-    }
-    auto splitted = split_2step(automaton, outs, true);
-    synt_measures.end_split_2step();
-
-    synt_measures.start_nba_to_dpa();
-    auto dpa = ntgba2dpa(splitted, gi.force_sbacc);
-    // Transform an automaton into a parity game by propagating players.
-    alternate_players(dpa);
-    // Merge states knows about players
-    dpa->merge_states();
-    set_synthesis_outputs(dpa, outs);
-    synt_measures.end_nba_to_dpa();
-
-    /**
-     * =================== Step 6: Solve the game and print the strategy
-     */
-    synt_measures.start_solve_game();
-    auto& arena = dpa;
-    bool is_solved = spot::solve_game(arena, gi);
-    synt_measures.end_solve_game();
-    if (!is_solved) {
-        synt_measures.completed();
-
-        cout << "/* Synthesis Measures: " << endl;
-        cout << synt_measures << endl;
-        cout << "*/" << endl;
-        cout << "UNREALIZABLE" << endl;
-
-        return EXIT_FAILURE;
-    }
-
-    synt_measures.start_dpa_to_mealy();
+    // Synthesis the input
     spot::mealy_like ml;
-    ml.success = spot::mealy_like::realizability_code::REALIZABLE_REGULAR;
-    ml.mealy_like = spot::solved_game_to_mealy(arena, gi);
-    bool should_split = false;
-    simplify_mealy_here(ml.mealy_like, synthesis_minimize_lvl, should_split);
-    synt_measures.end_dpa_to_mealy();
+    bool should_find_deps = !options.skip_dependencies;
+    bool is_realizable = synthesis_formula(synt_instance, gi, synt_measures, verbose,
+                                           should_find_deps, ml);
 
-    synt_measures.completed();
+    // Output results
     cout << "/* Synthesis Measures: " << endl;
     cout << synt_measures << endl;
     cout << "*/" << endl;
 
-    cout << "REALIZABLE" << endl;
-    spot::print_hoa(cout, ml.mealy_like);
-}
-
-// This code is taken from: spot/twaalgos/synthesis.cc
-twa_graph_ptr ntgba2dpa(const twa_graph_ptr& aut, bool force_sbacc) {
-    // if the input automaton is deterministic, degeneralize it to be sure to
-    // end up with a parity automaton
-    auto dpa = tgba_determinize(degeneralize_tba(aut), false, true, true, false);
-    dpa->merge_edges();
-    if (force_sbacc) dpa = sbacc(dpa);
-    reduce_parity_here(dpa, true);
-    assert(([&dpa]() -> bool {
-        bool max, odd;
-        return dpa->acc().is_parity(max, odd);
-    }()));
-    assert(is_deterministic(dpa));
-    return dpa;
+    if (is_realizable) {
+        cout << "REALIZABLE" << endl;
+        spot::print_hoa(cout, ml.mealy_like);
+    } else {
+        cout << "UNREALIZABLE" << endl;
+    }
 }
---
+++ src/synthesis_utils.h
@@ -0,0 +1,41 @@
+#ifndef REACTIVE_SYNTHESIS_BFSS_SYNTHESIS_UTILS_H
+#define REACTIVE_SYNTHESIS_BFSS_SYNTHESIS_UTILS_H
+
+#include <iostream>
+#include <spot/misc/optionmap.hh>
+#include <spot/twaalgos/degen.hh>
+#include <spot/twaalgos/determinize.hh>
+#include <spot/twaalgos/game.hh>
+#include <spot/twaalgos/hoa.hh>
+#include <spot/twaalgos/isdet.hh>
+#include <spot/twaalgos/mealy_machine.hh>
+#include <spot/twaalgos/sbacc.hh>
+#include <spot/twaalgos/sccfilter.hh>
+#include <spot/twaalgos/synthesis.hh>
+#include <spot/twaalgos/translate.hh>
+#include <vector>
+
+#include "automaton_algorithm.h"
+#include "synt_instance.h"
+#include "utils.h"
+
+spot::twa_graph_ptr ntgba2dpa(const spot::twa_graph_ptr& aut, bool force_sbacc);
+
+void remove_ap_from_automaton(const spot::twa_graph_ptr& automaton,
+                              vector<string>& variables);
+
+spot::twa_graph_ptr get_dpa_from_nba(spot::twa_graph_ptr nba, spot::synthesis_info& gi,
+                                     AutomatonSyntMeasure& synt_measures,
+                                     SyntInstance& synt_instance);
+
+spot::twa_graph_ptr get_nba_for_synthesis(SyntInstance& synt_instance,
+                                          spot::synthesis_info& gi,
+                                          AutomatonSyntMeasure& synt_measures,
+                                          std::ostream& verbose);
+
+// Return if realizable
+bool synthesis_formula(SyntInstance& synt_instance, spot::synthesis_info& gi,
+                       AutomatonSyntMeasure& synt_measures, std::ostream& verbose,
+                       bool find_deps, spot::mealy_like& ml);
+
+#endif
